70af32692612ddee8b2fe50906fa43a03f33eb08
phamtuankhoinodejs <huynhanhpham734@gmail.com>
2023-10-07 18:31:30 +0700

    connect db2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'db2',
+      host: 'localhost',
+      port: 50000,
+      username: 'db2admin',
+      password: 'db2admin',
+      database: 'demo',
+      models: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
